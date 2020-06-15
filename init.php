9871a27aad53a07a677e62b22a7cc02b2d55b418
Darko <miloradovic@outlook.com>
2020-06-15 16:46:56 +0200

    Implementirana sesija

--- init.php
+++ init.php
@@ -2,5 +2,11 @@
 include 'db/greske.php';
 include 'db/konekcija.php';
 include 'domen/Tablica.php';
-include 'domen/Vlasnik.php';
+include 'domen/Korisnik.php';
 include 'domen/Vozilo.php';
+
+session_start();
+
+if (!isset($_SESSION['ulogovaniKorisnik'])) {
+    $_SESSION['ulogovaniKorisnik'] = null;
+}
