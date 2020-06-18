a059fc11ec34c3f66695c5a60c03c28dcd6aa092
Darko <miloradovic@outlook.com>
2020-06-18 22:24:51 +0200

    Server side web zahtev

--- db/obrisiTablicu.php
+++ db/obrisiTablicu.php
@@ -4,9 +4,19 @@ include '../init.php';
 
 $id = $_GET['id'];
 
+$url = "http://seminarski.test/rest/tablice/" . $id;
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE"); 
+// curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+$odgovor = curl_exec($curl);
+$msg = json_decode($odgovor);
+curl_close($curl);
+/*
 $upit = "DELETE FROM reg_tablica WHERE id = $id";
 $mysqli->query($upit);
-
-header("Location: ../tablice.php?msg=Uspesno obrisana reg tablica");
+*/
+$string = "Location: ../tablice.php?msg=" . $msg->poruka;
+header($string);
 
  ?>
